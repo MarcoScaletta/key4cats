49d50e6c8930a5b3e8dab7207de559901ab39512
scal9000 <scaletta@cs.tu-darmstadt.de>
2025-10-12 23:56:18 +0200

    add option to just load proof obligation

--- cats/src/main/java/key4cats/KeY4CATs.java
+++ cats/src/main/java/key4cats/KeY4CATs.java
@@ -3,17 +3,14 @@ package key4cats;
 
 import de.uka.ilkd.key.control.DefaultUserInterfaceControl;
 import de.uka.ilkd.key.control.KeYEnvironment;
-import de.uka.ilkd.key.core.Log;
 import de.uka.ilkd.key.gui.MainWindow;
 import de.uka.ilkd.key.gui.WindowUserInterfaceControl;
 import de.uka.ilkd.key.proof.io.ProblemLoaderException;
 import org.apache.commons.cli.HelpFormatter;
 
 import java.io.*;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static de.uka.ilkd.key.core.Main.loadCommandLineFiles;
 
@@ -26,6 +23,8 @@ public class KeY4CATs {
 
     public enum ProofGenMode{SINGLE,FULL,ALL}
 
+    public enum LoadingMode{CAT, PO}
+
 
     public static final PrintStream unmutedOut = System.out;
     public static final PrintStream mutedOut = new PrintStream(OutputStream.nullOutputStream());
@@ -33,14 +32,11 @@ public class KeY4CATs {
     enum KeYMode {AUTO, GUI}
 
     static Option HELP_OPTION = Option.builder("h")
-        .required(true)
         .desc("Shows this help menu")
         .longOpt("help")
         .build();
 
-
     static Option FULL_PROOF = Option.builder("f")
-            .required(true)
             .desc("Targets <CAT_ID> and also all contracts assumed by <CAT_ID> from file <CATSL_FILE>")
             .longOpt("full")
             .hasArg().argName("CAT_ID")
@@ -52,19 +48,11 @@ public class KeY4CATs {
             .longOpt("statistics")
             .build();
 
-
     static Option SINGLE_CAT = Option.builder("s")
-//            .required(true)
             .desc("Targets single contract <CAT_ID> from file <CATSL_FILE>")
             .longOpt("single")
             .numberOfArgs(2)
             .hasArg().argName("CAT_ID")
-            .build();
-
-    static Option ALL_CATS = Option.builder("all")
-//            .required(true)
-            .desc("Targets all CATs defined in <CATSL_FILE>")
-            .longOpt("all-cats")
             .build();
 
     static Option CATSL = Option.builder("catsl")
@@ -75,7 +63,6 @@ public class KeY4CATs {
             .build();
 
     static Option NO_VER = Option.builder("no-ver")
-//            .required(true)
             .desc("Only generates Proof Obligations (.key files)")
             .longOpt("no-verification")
             .build();
@@ -92,53 +79,41 @@ public class KeY4CATs {
             .hasArg().argName("JAVA_CLASS")
             .build();
 
-    static final OptionGroup TARGET_OPTION =
-            new OptionGroup(){
-                @Override
-                public String toString() {
-                    StringBuilder buff = new StringBuilder();
-                    Iterator<Option> iter = this.getOptions().iterator();
-                    buff.append("[");
-                    while(iter.hasNext()) {
-                        Option option = iter.next();
-                        if (option.getOpt() != null) {
-                            buff.append("-");
-                            buff.append(option.getOpt());
-                        } else {
-                            buff.append("--");
-                            buff.append(option.getLongOpt());
-                        }
-
-                        if (option.getArgName() != null) {
-                            buff.append(' ');
-                            buff.append(option.getArgName());
-                        }
-
-                        if (iter.hasNext()) {
-                            buff.append(", ");
-                        }
-                    }
-                    buff.append("]");
-                    return buff.toString();
-                }
-    }.addOption(SINGLE_CAT).addOption(FULL_PROOF).addOption(ALL_CATS);
+    static Option PROOF_OBLIGATION = Option.builder("po")
+            .desc("Load <PROOF_OBLIGATION> (.key/.proof file)")
+            .longOpt("proof-obligation")
+            .hasArg().argName("<PROOF_OBLIGATION>")
+            .build();
 
 
+    static final OptionGroup TARGET_OPTION = new MyOptionGroup().addOption(SINGLE_CAT).addOption(FULL_PROOF);
+    static final OptionGroup MAIN_OPTION_GROUP = new MyOptionGroup().addOption(CATSL).addOption(PROOF_OBLIGATION).addOption(HELP_OPTION);
 
 
-    static final Options options = new Options();
+    static final Options loadPOOptions = new Options();
+    static final Options mainOptions = new Options();
+    static final Options loadCATOptions = new Options();
     static final Options helpOption = new Options();
-    static {
-        TARGET_OPTION.setRequired(true);
 
-        options.addOptionGroup(TARGET_OPTION);
-        options.addOption(NO_VER);
-        options.addOption(CATSL);
-        options.addOption(JAVA_CLASS);
-        options.addOption(INTERACTIVE);
-        options.addOption(SHOW_STATS);
+    static {
+        MAIN_OPTION_GROUP.setRequired(true);
+        mainOptions.addOptionGroup(MAIN_OPTION_GROUP);
+
+        loadCATOptions.addOptionGroup(TARGET_OPTION);
+        loadCATOptions.addOption(NO_VER);
+        loadCATOptions.addOption(CATSL);
+        loadCATOptions.addOption(JAVA_CLASS);
+        loadCATOptions.addOption(INTERACTIVE);
+        loadCATOptions.addOption(SHOW_STATS);
+        loadCATOptions.addOption(HELP_OPTION);
+
+
+        loadPOOptions.addOption(PROOF_OBLIGATION);
+        loadPOOptions.addOption(INTERACTIVE);
+        loadPOOptions.addOption(SHOW_STATS);
 
         helpOption.addOption(HELP_OPTION);
+
     }
 
     static ProofGenMode proofGenMode;
@@ -146,8 +121,10 @@ public class KeY4CATs {
     static String contractName;
     static String catsFilename;
     static String javaClassFilename;
+    static String proofObligationFileName;
     static String keyHome = System.getenv("KEY");
     static KeYMode executionMode = KeYMode.AUTO;
+    static LoadingMode loadingMode;
     static boolean stats = false;
     static String directory;
 
@@ -155,31 +132,38 @@ public class KeY4CATs {
     public static void main(String [] args) throws IOException {
         checkOptions(args);
         checkOptions();
-        File catsFile = new File(keyHome + catsFilename);
-        String directory = catsFile.getParent();
-        System.out.println(directory);
         try {
-            ProofCATsBuilder p = new ProofCATsBuilder(catsFile, contractName, javaClassFilename, proofGenMode);
-            Set<String> contractNames = p.getContractIds();
-            if(!contractNames.iterator().hasNext())
-                throw new RuntimeException( "No contract to be proven (check what command you run)");
-            if(proofGenMode == ProofGenMode.SINGLE) {
-                LOGGER.info(String.format("Single proof for \"%s\"", contractName));
-                p.generateProof(directory,contractName);
-
-                if(requiredVerification)
-                    prove(directory, contractName);
+            if(loadingMode == LoadingMode.PO){
+                directory = new File(proofObligationFileName).getParent();
+                LOGGER.info(String.format("Loading proof obligation from %s", proofObligationFileName));
+                prove(keyHome + proofObligationFileName);
             }
-            if(proofGenMode == ProofGenMode.FULL){
-                if(contractNames.size() == 1)
-                    LOGGER.warn(String.format("Contract '%s' does not assume any contract. Use '-s' option instead of '-f'.",contractNames.stream().findFirst().get()));
-                LOGGER.info(String.format("Full proof for %s. %s contracts to be proven: %s ", contractName, contractNames.size(), contractNames));
-                multipleContracts(p,directory,contractNames);
+            else {
+                File catsFile = new File(keyHome + catsFilename);
+                directory = catsFile.getParent();
+                System.out.println(directory);
+                ProofCATsBuilder p = new ProofCATsBuilder(catsFile, contractName, javaClassFilename, proofGenMode);
+                Set<String> contractNames = p.getContractIds();
+                if(!contractNames.iterator().hasNext())
+                    throw new RuntimeException( "No contract to be proven (check what command you run)");
+                if (proofGenMode == ProofGenMode.SINGLE) {
+                    LOGGER.info(String.format("Single proof for \"%s\"", contractName));
+                    p.generateProof(directory, contractName);
 
-            }
-            if(proofGenMode == ProofGenMode.ALL){
-                LOGGER.info(String.format("Prove all %s contracts from file %s", contractNames.size(), catsFilename));
-                multipleContracts(p,directory,contractNames);
+                    if (requiredVerification)
+                        prove(directory, contractName);
+                }
+                if (proofGenMode == ProofGenMode.FULL) {
+                    if (contractNames.size() == 1)
+                        LOGGER.warn(String.format("Contract '%s' does not assume any contract. Use '-s' option instead of '-f'.", contractNames.stream().findFirst().get()));
+                    LOGGER.info(String.format("Full proof for %s. %s contracts to be proven: %s ", contractName, contractNames.size(), contractNames));
+                    multipleContracts(p, directory, contractNames);
+
+                }
+                if (proofGenMode == ProofGenMode.ALL) {
+                    LOGGER.info(String.format("Prove all %s contracts from file %s", contractNames.size(), catsFilename));
+                    multipleContracts(p, directory, contractNames);
+                }
             }
         }catch(RuntimeException e){
             System.err.print(e);
@@ -209,7 +193,21 @@ public class KeY4CATs {
         }
     }
 
-    private static boolean prove(String directory, String contractName) throws IOException{
+    private static boolean prove(String proofObligationFileName) {
+        if(executionMode == KeYMode.GUI) {
+            LOGGER.info("Starting Interactive Mode (continue in the newly opened window)");
+            openFileWithGUI(proofObligationFileName);
+            return false;
+        }
+        else if(executionMode == KeYMode.AUTO) {
+            LOGGER.info("Starting Auto Mode");
+            return openFileWithCLI(proofObligationFileName);
+        }else {
+            throw new RuntimeException("Execution Mode should be GUI or AUTO but found: "+ executionMode);
+        }
+    }
+
+    private static boolean prove(String directory, String contractName) {
         if(executionMode == KeYMode.GUI) {
             LOGGER.info("Starting Interactive Mode (continue in the newly opened window)");
             openFileWithGUI(directory, contractName);
@@ -231,24 +229,44 @@ public class KeY4CATs {
     private static void checkOptions(String[] args){
         CommandLine cl;
         CommandLineParser parser = new DefaultParser();
+        Set<String> argsSet = Arrays.stream(args).collect(Collectors.toSet());
 
+        System.err.println(argsSet);
+        System.err.println(argsSet.contains("-" + PROOF_OBLIGATION.getOpt()));
         try {
-            parser.parse(helpOption, args);
-            printHelp();
-            System.exit(0);
-        }catch (ParseException e1){
-            try {
-                cl = parser.parse(options, args);
-                parseArgs(cl);
-            }catch (ParseException e){
+            if(argsSet.contains("-" + HELP_OPTION.getOpt()) || argsSet.contains("--" + HELP_OPTION.getLongOpt())) {
+                printHelp();
+            }
+            else if(argsSet.contains("-" + PROOF_OBLIGATION.getOpt()) || argsSet.contains("--" + PROOF_OBLIGATION.getLongOpt())) {
+                try{
+                    cl = parser.parse(loadPOOptions, args);
+                    loadingMode = LoadingMode.PO;
+                    parseArgs(cl);
+                }catch (ParseException e){
                     System.err.println("Problem with arguments, Type 'cats --help'");
                     throw new RuntimeException("Cannot parse arguments\n" + e.getMessage());
+                }
             }
+            else if(argsSet.contains("-" + CATSL.getOpt()) || argsSet.contains("--" + CATSL.getLongOpt())) {
+                cl = parser.parse(loadCATOptions, args);
+                loadingMode = LoadingMode.CAT;
+                parseArgs(cl);
+            }else
+                throw new RuntimeException(String.format("Missing required options:\n\t" +
+                        "Include '-catsl' for loading CATs (.cats) or '-po' to load proof obligations (.key, .proof)\n%s", "Type 'cats --help' for help"));
+        }catch (ParseException e){
+            System.err.println("Problem with arguments, Type 'cats --help'");
+            throw new RuntimeException("Cannot parse arguments\n" + e.getMessage());
+
         }
     }
 
     private static void printHelp(){
         HelpFormatter formatter = new HelpFormatter();
+        Options options = new Options();
+        options.addOptions(loadCATOptions);
+        options.addOptions(loadPOOptions);
+        options.addOptions(helpOption);
         formatter.setWidth(120);
         PrintWriter pw = new PrintWriter(System.out);
         formatter.setOptionComparator(null);
@@ -266,34 +284,44 @@ public class KeY4CATs {
 
     private static void parseArgs(CommandLine cl){
         requiredVerification = !cl.hasOption(NO_VER);
-        if(cl.hasOption(SINGLE_CAT)) {
-            contractName = cl.getOptionValue(SINGLE_CAT);
-            proofGenMode = ProofGenMode.SINGLE;
+        if(loadingMode == LoadingMode.CAT){
+            if(cl.hasOption(SINGLE_CAT)) {
+                contractName = cl.getOptionValue(SINGLE_CAT);
+                proofGenMode = ProofGenMode.SINGLE;
+            }
+            if(cl.hasOption(FULL_PROOF)) {
+                contractName = cl.getOptionValue(FULL_PROOF);
+                proofGenMode = ProofGenMode.FULL;
+            }
+            if(!cl.hasOption(SINGLE_CAT) && (!cl.hasOption(FULL_PROOF)))
+                proofGenMode = ProofGenMode.ALL;
+
+            if(cl.hasOption(CATSL)){
+                catsFilename = cl.getOptionValue(CATSL);
+            }
+            if(cl.hasOption(JAVA_CLASS)){
+                javaClassFilename = cl.getOptionValue(JAVA_CLASS);
+            }
         }
-        if(cl.hasOption(FULL_PROOF)) {
-            contractName = cl.getOptionValue(FULL_PROOF);
-            proofGenMode = ProofGenMode.FULL;
+        if(loadingMode == LoadingMode.PO){
+            if(cl.hasOption(PROOF_OBLIGATION)) {
+                proofObligationFileName = cl.getOptionValue(PROOF_OBLIGATION);
+            }
         }
         if(cl.hasOption(INTERACTIVE)) {
             executionMode = KeYMode.GUI;
         }
-        if(cl.hasOption(ALL_CATS)) {
-            proofGenMode = ProofGenMode.ALL;
-        }
-        if(cl.hasOption(CATSL)){
-            catsFilename = cl.getOptionValue(CATSL);
-        }
-        if(cl.hasOption(JAVA_CLASS)){
-            javaClassFilename = cl.getOptionValue(JAVA_CLASS);
-        }
+        stats = cl.hasOption(SHOW_STATS);
     }
 
-
-
-    private static void openFileWithGUI(String directory, String contractName) {
-        File keyFile = new File(String.format("%s/%s.key", directory, contractName));
+    private static void openFileWithGUI(String proofObligationFileName) {
+        File keyFile = new File(proofObligationFileName);
         WindowUserInterfaceControl windowUserInterfaceControl = MainWindow.getInstance().getUserInterface();
         loadCommandLineFiles(windowUserInterfaceControl, List.of(keyFile));
+    }
+
+    private static void openFileWithGUI(String directory, String contractName) {
+        openFileWithGUI(String.format("%s/%s.key", directory, contractName));
     }
 
     private static void muteOut(){
@@ -303,10 +331,10 @@ public class KeY4CATs {
         System.setOut(unmutedOut);
     }
 
-    private static boolean openFileWithCLI(String directory, String contractName) {
-        try {
-            File keyFile = new File(String.format("%s/%s.key", directory, contractName));
-            LOGGER.info(String.format("Loading %s/%s.key ...", directory, contractName));
+    private static boolean openFileWithCLI(String proofObligationFileName){
+        try{
+            File keyFile = new File(proofObligationFileName);
+            LOGGER.info(String.format("Loading %s ...", proofObligationFileName));
             KeYEnvironment<DefaultUserInterfaceControl> env = KeYEnvironment.load(keyFile);
             LOGGER.info("Started proof...");
             env.getProofControl().startAndWaitForAutoMode(env.getLoadedProof());
@@ -323,6 +351,10 @@ public class KeY4CATs {
         }
     }
 
+    private static boolean openFileWithCLI(String directory, String contractName) {
+        return openFileWithCLI(String.format("%s/%s.key", directory, contractName));
+    }
+
 
 
     static String red(String s){
@@ -332,4 +364,34 @@ public class KeY4CATs {
         return "\033[32m " + s + "\033[0m ";
     }
 
+}
+
+class MyOptionGroup extends OptionGroup {
+    @Override
+    public String toString() {
+        StringBuilder buff = new StringBuilder();
+        Iterator<Option> iter = this.getOptions().iterator();
+        buff.append("[");
+        while(iter.hasNext()) {
+            Option option = iter.next();
+            if (option.getOpt() != null) {
+                buff.append("-");
+                buff.append(option.getOpt());
+            } else {
+                buff.append("--");
+                buff.append(option.getLongOpt());
+            }
+
+            if (option.getArgName() != null) {
+                buff.append(' ');
+                buff.append(option.getArgName());
+            }
+
+            if (iter.hasNext()) {
+                buff.append(", ");
+            }
+        }
+        buff.append("]");
+        return buff.toString();
+    }
 }
