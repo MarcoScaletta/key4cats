4de2ac41e1881b363a49332df132b85fb05b783f
scal9000 <scaletta@cs.tu-darmstadt.de>
2026-06-18 20:43:27 +0200

    update message benchmark removeOneQuadCAT

--- experiments/3-internal-behavior/benchmark-removeOneQuadCAT.sh
+++ experiments/3-internal-behavior/benchmark-removeOneQuadCAT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Verifying removeOneQuadCAT WITHOUT JVM warmup"
-echo "\033[1;31m !! The following proof can take VERY LONG (98 minutes originally) !!\033[0m"
+echo "\033[1;31m !! The following proof can take VERY LONG (>1.5h minutes originally) !!\033[0m"
 echo "\033[1;31m !! JVM warmup for this CAT is unnecessary and NOT RECOMMENDED !!\033[0m"
 echo "The proof will be done interactively (remove -i, otherwise)"
 key4cats -i -s removeOneQuadCAT -catsl removeOneCaseStudy.cats -java RemoveOneCaseStudy -max 40000
