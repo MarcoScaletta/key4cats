3326612cfb667239a6510eead0fde1cd5fbecf25
scal9000 <scaletta@cs.tu-darmstadt.de>
2026-06-18 12:52:32 +0200

    add more sanity checks

--- sanity-checks/3-check-run-example.sh
+++ sanity-checks/3-check-run-example.sh
@@ -2,3 +2,5 @@
 
 echo "Checking verification process for trivial example: proof should succeed."
 key4cats -s sanityCheckExampleCAT -catsl sanityCheckExample.cats -java SanityCheckExample
+key4cats -f sanityCheckExampleCAT -catsl sanityCheckExample.cats -java SanityCheckExample
+key4cats -catsl sanityCheckExample.cats -java SanityCheckExample
