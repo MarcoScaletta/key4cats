07374ae655c1e43cc970058173cdb8fbb076d1af
scal9000 <scaletta@cs.tu-darmstadt.de>
2026-06-20 15:46:42 +0200

    add generation of parser from gradle build

--- cats/generate-parsers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-grammars=("CATs")
-echo "Generating parsers:" 
-for grammar in "${grammars[@]}"; do
-    echo \\t ">" ${grammar}
-    antlr4 cats/grammars/${grammar}.g4 -v 4.13.2 -o  cats/src/main/java/key4cats/parsers/${grammar} -Xexact-output-dir -package key4cats.parsers.${grammar} -visitor
-done
