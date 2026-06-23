2dcceb414a2fb87e01c85ff2a5f74718dd958188
scal9000 <scaletta@cs.tu-darmstadt.de>
2026-06-23 17:44:06 +0200

    ++

--- examples-reusability/1-specification-reuse/addOneCAT-reuse/verify-addOneCAT-reuse.sh
+++ examples-reusability/1-specification-reuse/addOneCAT-reuse/verify-addOneCAT-reuse.sh
@@ -3,4 +3,4 @@
 echo "Verifying full file specs-file-addOne.cats"
 printf "> EXPECTED OUTCOME for addOneCAT and addTwoCAT: \033[32m SUCCESS\033[0m\n"
 printf "> EXPECTED OUTCOME for doNothingCAT: \033[31m FAIL\033[0m\n"
-key4cats -catsl addOne-reuse.cats -java SpecsReuse
+key4cats -catsl addOneCAT-reuse.cats -java SpecsReuse
