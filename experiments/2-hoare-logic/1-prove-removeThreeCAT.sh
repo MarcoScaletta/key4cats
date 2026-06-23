912e5ba517e4f770541ea2fe918856b4beeeff11
scal9000 <scaletta@cs.tu-darmstadt.de>
2026-06-23 17:02:31 +0200

    ++

--- experiments/2-hoare-logic/1-prove-removeThreeCAT.sh
+++ experiments/2-hoare-logic/1-prove-removeThreeCAT.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Verifying removeThreeCAT and all its dependencies"
-printf "> EXPECTED OUTCOME: \033[32m SUCCESS\033[0m\n"
+printf "> EXPECTED OUTCOME FOR ALL CATs: \033[32m SUCCESS\033[0m\n"
 key4cats -f removeThreeCAT -catsl hoare-logic.cats -java HoareLogic -b
